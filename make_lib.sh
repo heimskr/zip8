3c10320d2a2ef84b34f3fdf31b6294ba800672f7
Kai Tamkun <ktamkun@ucsc.edu>
2024-10-25 01:38:48 -0700

    Optimize!

--- make_lib.sh
+++ make_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MOVE_TARGET=$(realpath $2)
 cd $(dirname $1)
-zig build && mv zig-out/lib/libzip8.a "$MOVE_TARGET"
+zig build -Doptimize=ReleaseFast && mv zig-out/lib/libzip8.a "$MOVE_TARGET"
